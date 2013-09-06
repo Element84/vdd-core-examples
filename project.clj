740bc61b762dfe13cb26bc66c3d24af90fa0ac0e
Jason Gilman <jason@element84.com>
2013-09-06 16:33:25 -0400

    Updating dependency to use correct group name for vdd-core.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject vdd-core-examples "0.1.0-SNAPSHOT"
+(defproject element84/vdd-core-examples "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [vdd-core "0.1.0"]]
+                 [element84/vdd-core "0.1.0"]]
   
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.3"]]
                    ; Add viz as a source path to load visualization drivers
